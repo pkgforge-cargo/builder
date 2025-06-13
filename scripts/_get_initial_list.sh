85c05ffea1fbccdcca3546a6166cb2e0a0bf6dbb
Azathothas <AjamX101@gmail.com>
2025-06-13 12:54:42 +0545

    saner

--- scripts/_get_initial_list.sh
+++ scripts/_get_initial_list.sh
@@ -22,7 +22,7 @@ echo -e "\n[+] Using dir: ${TEMP_DIR}\n"
              .crates[] | 
              def clean_strings: walk(if type == "string" then gsub("\\n|\\r|\\t"; " ") | gsub("\\s+"; " ") | gsub("^\\s+|\\s+$"; "") else . end);
              {
-                 pkg: .name?,
+                 name: .name?,
                  description: .description?,
                  version: (.newest_version? // .max_version? // .version?),
                  homepage: (.repository? // .homepage? // .documentation?),
@@ -54,7 +54,7 @@ echo -e "\n[+] Using dir: ${TEMP_DIR}\n"
              .crates[] | 
              def clean_strings: walk(if type == "string" then gsub("\\n|\\r|\\t"; " ") | gsub("\\s+"; " ") | gsub("^\\s+|\\s+$"; "") else . end);
              {
-                 pkg: .name?,
+                 name: .name?,
                  description: .description?,
                  version: (.newest_version? // .max_version? // .version?),
                  homepage: (.repository? // .homepage? // .documentation?),
@@ -77,11 +77,11 @@ echo -e "\n[+] Using dir: ${TEMP_DIR}\n"
  find "${TEMP_DIR}" -type f -size -3c -delete
  find "${TEMP_DIR}" -type f -iname "*.json" -exec cat "{}" + > "${TEMP_DIR}/RAW.json.tmp"
  awk '/^\s*{\s*$/{flag=1; buffer="{\n"; next} /^\s*}\s*$/{if(flag){buffer=buffer"}\n"; print buffer}; flag=0; next} flag{buffer=buffer$0"\n"}' "${TEMP_DIR}/RAW.json.tmp" | jq -c '. as $line | (fromjson? | .message) // $line' >> "${TEMP_DIR}/RAW.json.raw"
- jq -s '[.[] | select(type == "object" and has("pkg"))] 
-       | unique_by(.pkg | ascii_downcase) 
-       | sort_by(.pkg | ascii_downcase)' "${TEMP_DIR}/RAW.json.raw" > "${TEMP_DIR}/RAW.json"
+ jq -s '[.[] | select(type == "object" and has("name"))] 
+       | unique_by(.name | ascii_downcase) 
+       | sort_by(.name | ascii_downcase)' "${TEMP_DIR}/RAW.json.raw" > "${TEMP_DIR}/RAW.json"
 #Process
- echo -e "\n[+] Processing Crates [$(jq -r '.[] | .pkg' '/tmp/crates/RAW.json' | wc -l)] ...\n"
+ echo -e "\n[+] Processing Crates [$(jq -r '.[] | .name' '/tmp/crates/RAW.json' | wc -l)] ...\n"
  rm -rf "/tmp/crates" 2>/dev/null
  mkdir -p "/tmp/crates"
  process_crate() {
@@ -100,23 +100,23 @@ echo -e "\n[+] Using dir: ${TEMP_DIR}\n"
      done
      #Add has_bins field
      jq --arg pkg "$pkg" --arg has_bins "$has_bins" \
-         '.[] | select(.pkg == $pkg) | . + {has_bins: ($has_bins == "true")}' \
+         '.[] | select(.name == $pkg) | . + {has_bins: ($has_bins == "true")}' \
          RAW.json > "/tmp/crates/${pkg}.json"
      
      echo "Processed: $pkg (has_bins: $has_bins)"
  }
  export -f process_crate
  #too many will ratelimit us
- jq -r '.[] | .pkg' "${TEMP_DIR}/RAW.json" | xargs -P 5 -I {} bash -c 'process_crate "{}"'
+ jq -r '.[] | .name' "${TEMP_DIR}/RAW.json" | xargs -P 5 -I {} bash -c 'process_crate "{}"'
 #Merge Again
  find "/tmp/crates" -type f -size -3c -delete
  find "/tmp/crates" -type f -iname "*.json" -exec cat "{}" + > "/tmp/crates/RAW.json.tmp"
  awk '/^\s*{\s*$/{flag=1; buffer="{\n"; next} /^\s*}\s*$/{if(flag){buffer=buffer"}\n"; print buffer}; flag=0; next} flag{buffer=buffer$0"\n"}' "/tmp/crates/RAW.json.tmp" | jq -c '. as $line | (fromjson? | .message) // $line' >> "/tmp/crates/RAW.json.raw"
- jq -s '[.[] | select(type == "object" and has("pkg"))] | unique_by(.pkg | ascii_downcase) | sort_by(.pkg | ascii_downcase) | walk(if type == "object" then with_entries(select(.value != null and .value != "" and .value != "null")) elif type == "boolean" or type == "number" then tostring else . end) | map(to_entries | sort_by(.key) | from_entries)' "/tmp/crates/RAW.json.raw" |\
+ jq -s '[.[] | select(type == "object" and has("name"))] | unique_by(.name | ascii_downcase) | sort_by(.name | ascii_downcase) | walk(if type == "object" then with_entries(select(.value != null and .value != "" and .value != "null")) elif type == "boolean" or type == "number" then tostring else . end) | map(to_entries | sort_by(.key) | from_entries)' "/tmp/crates/RAW.json.raw" |\
  jq '
   sort_by([
     -(if .downloads then (.downloads | tonumber) else -1 end),
-    .pkg
+    .name
   ]) |
   to_entries |
   map(.value + { rank: (.key + 1 | tostring) })
@@ -125,15 +125,15 @@ echo -e "\n[+] Using dir: ${TEMP_DIR}\n"
   jq 'map(select(.has_bins == "true"))' "/tmp/crates/CRATES_DUMP.json" |\
   jq 'walk(if type == "boolean" or type == "number" then tostring else . end)' |\
   jq 'map(select(
-     .pkg != null and .pkg != "" and
+     .name != null and .name != "" and
      .has_bins != null and .has_bins != "" and
      .version != null and .version != ""
-  ))' | jq 'unique_by(.pkg) | sort_by(.rank | tonumber) | [range(length)] as $indices | [., $indices] | transpose | map(.[0] + {rank: (.[1] + 1 | tostring)})' > "/tmp/crates/CRATES_BIN_ONLY.json"
+  ))' | jq 'unique_by(.name) | sort_by(.rank | tonumber) | [range(length)] as $indices | [., $indices] | transpose | map(.[0] + {rank: (.[1] + 1 | tostring)})' > "/tmp/crates/CRATES_BIN_ONLY.json"
 #Print stats
  du -bh "/tmp/crates/CRATES_DUMP.json"
  du -bh "/tmp/crates/CRATES_BIN_ONLY.json"
- echo -e "\n[+] Total Packages: $(jq -r '.[] | .pkg' '/tmp/crates/CRATES_DUMP.json' | wc -l)"
- echo -e "[+] Binary Packages: $(jq -r '.[] | .pkg' '/tmp/crates/CRATES_BIN_ONLY.json' | wc -l)\n"
+ echo -e "\n[+] Total Packages: $(jq -r '.[] | .name' '/tmp/crates/CRATES_DUMP.json' | wc -l)"
+ echo -e "[+] Binary Packages: $(jq -r '.[] | .name' '/tmp/crates/CRATES_BIN_ONLY.json' | wc -l)\n"
 #Cleanup
 popd &>/dev/null
 #-------------------------------------------------------#
